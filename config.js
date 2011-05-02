38170152f6885285164df2745ceeb297fcec9d3f
Todd Fisher <todd.fisher@gmail.com>
2011-05-02 13:36:05 -0400

    work out a solution to have child process die if the master crashed

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 exports.load = function() {
+  var path  = require("path");
+  var fs    = require("fs");
+  var posix = require(__dirname + "/build/default/posixtools.node");
+
   return {
     tcp: 'tcp4',
     backlog: 128,
@@ -12,14 +16,28 @@ exports.load = function() {
     stderr_path: __dirname + "/stderr.log",
     stdout_path: __dirname + "/stdout.log",
     pidfile: __dirname + "/pidfile.pid",
-    before_exec: function() {
+    before_exec: function(config) {
       console.error("before exec");
     },
-    before_fork: function() {
+    before_fork: function(config, wid) {
       console.error("before fork");
     },
-    after_fork: function() {
-      console.error("after fork");
+    after_fork: function(config, pid, ppid, wid) {
+      console.error("after fork: %d", process.pid);
+      var oldbin = config.pidfile + ".oldbin";
+      if (path.existsSync(oldbin)) {
+        var oldpid = fs.readFileSync(oldbin, 'ascii');
+        if (parseInt(oldpid) > 1) {
+          console.error("send(%d) old master the kill sig: %s", posix.getpid(), oldpid);
+          process.kill(parseInt(oldpid), "SIGQUIT");
+        }
+        else {
+          console.error("nopid in oldbin: %s", oldpid);
+        }
+      }
+      else {
+        console.error("no oldbin");
+      }
     }
   }
 };
