8c9056f7adc891516ed19eb84fac159957767cd4
Todd Fisher <todd.fisher@gmail.com>
2011-05-01 22:46:28 -0400

    allow USR2 to reexec new bin, QUIT to close socket

--- src/fork.cc
+++ src/fork.cc
@@ -1,6 +1,6 @@
 /*
-* Fork.node: A node.JS addon to add process.fork for *nix
-* dervied from daemon.node
+* posixtools.node: A node.JS addon to add useful tools for creating *nix servers
+* dervied from daemon.node, unicorn
 *
 * Copyright 2011 (c) <todd>
 *
@@ -20,6 +20,7 @@
 #include <pwd.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/socket.h>
 
 #define PID_MAXLEN 10
 
@@ -31,9 +32,12 @@ using namespace node;
 //
 static Handle<Value> Fork(const Arguments& args) {
   HandleScope scope;
-
-  pid_t sid, pid = fork();
+  pid_t sid, pid;
   int i, new_fd;
+
+  pid = fork();
+  // http://www.unixguide.net/unix/programming/1.1.2.shtml
+  // vfork really isn't necessary... use fork
 
   if (pid < 0)      exit(1);
 
@@ -55,6 +59,107 @@ static Handle<Value> Fork(const Arguments& args) {
   }
 
   return scope.Close(Number::New(pid));
+}
+static Handle<Value> SetSocketOpts(const Arguments& args) {
+  HandleScope scope;
+  int flags = 1;
+  int r;
+  int fd = args[0]->ToInteger()->Value();
+
+  if ((r=setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (void *)&flags, sizeof(flags))) < 0) { 
+    fprintf(stderr, "setsockopt(error): %d, %d:%s\n", r, errno, strerror(errno));
+  }
+
+  return scope.Close(Number::New(r));
+}
+static Handle<Value> Dup2(const Arguments& args) {
+  HandleScope scope;
+  int flags = 1;
+  int r;
+  int fd1 = args[0]->ToInteger()->Value();
+  int fd2 = args[1]->ToInteger()->Value();
+
+  if ((r=dup2(fd1, fd2)) < 0) { 
+    fprintf(stderr, "dup2(error): %d, %d:%s\n", r, errno, strerror(errno));
+    return ThrowException(Exception::Error(String::New("dup2 error")));
+  }
+
+  return scope.Close(Number::New(r));
+}
+//
+//  execve(const char *path, char *const argv[], char *const envp[]);
+//
+// see: man 2 execve
+//
+// some of this implementation is borrowed by reference to node_child_process.cc
+static Handle<Value> Execve(const Arguments& args) {
+  HandleScope scope;
+
+  if (args.Length() != 3 || 
+      !args[0]->IsString() ||
+      !args[1]->IsArray() ||
+      !args[2]->IsArray()) {
+    fprintf(stderr, "args.Length: %d, %d, %d, %d\n", args.Length(), args[0]->IsString(), args[1]->IsArray(), args[2]->IsArray());
+    return ThrowException(Exception::Error(String::New("Bad Arguments, expected: path, argv[], envp[]")));
+  }
+
+  int i;
+  String::Utf8Value file(args[0]->ToString());
+  // Copy arguments args[1] and args[2] into a c-string array called argv.
+  // The array must be null terminated, and the first element must be
+  // the name of the executable -- hence the complication.
+  Local<Array> argv_handle = Local<Array>::Cast(args[1]);
+  Local<Array> envp_handle = Local<Array>::Cast(args[2]);
+
+  // setup argv
+  int argc = argv_handle->Length();
+  int argv_length = argc + 1 + 1;
+  char **argv = new char*[argv_length]; // heap allocated to detect errors
+  argv[0] = strdup(*file); // + 1 for file
+  argv[argv_length-1] = NULL;  // + 1 for NULL;
+  for (i = 0; i < argc; ++i) {
+    String::Utf8Value arg(argv_handle->Get(Integer::New(i))->ToString());
+    argv[i+1] = strdup(*arg);
+  }
+
+  // setup env
+  int envc = envp_handle->Length();
+  char **envp = new char*[envc+1];
+  envp[envc] = NULL; // + 1 for NULL;
+  for (i = 0; i < envc; ++i) {
+    String::Utf8Value env(envp_handle->Get(Integer::New(i))->ToString());
+    envp[i] = strdup(*env);
+  }
+  fprintf(stderr, "execute file: %s\n", *file);
+
+  int r = execve(*file, argv, envp);
+
+  // return's only on error
+  return ThrowException(Exception::Error(String::New("Execve failed!")));
+}
+
+// set the fd to close on a call to exec
+static Handle<Value> FdCloseOnExec(const Arguments& args) {
+  HandleScope scope;
+  if (!args[0]->IsNumber()) {
+    return ThrowException(Exception::Error(String::New("When setting a umask it must be an integer")));
+  }
+  int fd = args[0]->ToInteger()->Value();
+  int flags = fcntl(fd, F_GETFD, 0);
+
+  return scope.Close(Number::New(fcntl(fd, F_SETFD, flags | FD_CLOEXEC)));
+}
+// set the fd to stay open after an exec call
+static Handle<Value> FdOpenOnExec(const Arguments& args) {
+  HandleScope scope;
+  if (!args[0]->IsNumber()) {
+    return ThrowException(Exception::Error(String::New("When setting a umask it must be an integer")));
+  }
+  int fd = args[0]->ToInteger()->Value();
+  int flags = fcntl(fd, F_GETFD, 0);
+
+  
+  return scope.Close(Number::New(fcntl(fd, F_SETFD, flags & ~FD_CLOEXEC)));
 }
 
 //
@@ -138,4 +243,9 @@ extern "C" void init(Handle<Object> target) {
   NODE_SET_METHOD(target, "umask", Umask);
   NODE_SET_METHOD(target, "isalive", IsAlive);
   NODE_SET_METHOD(target, "getppid", GetPPid);
+  NODE_SET_METHOD(target, "fd_close_on_exec", FdCloseOnExec);
+  NODE_SET_METHOD(target, "fd_open_on_exec", FdOpenOnExec);
+  NODE_SET_METHOD(target, "execve", Execve);
+  NODE_SET_METHOD(target, "set_socket_opts", SetSocketOpts);
+  NODE_SET_METHOD(target, "dup2", Dup2);
 }
