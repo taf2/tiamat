e003120553f5687a09deb02f7a4a687e51a01f6a
Todd Fisher <todd.fisher@gmail.com>
2011-07-02 16:34:17 -0400

    use oldbinQuit by default if no after_fork is defined

--- config.js
+++ config.js
@@ -16,17 +16,7 @@ exports.load = function() {
     working_directory: __dirname,
     stderr_path: __dirname + "/stderr.log",
     stdout_path: __dirname + "/stdout.log",
-    pidfile: __dirname + "/pidfile.pid",
-    before_exec: function(config) {
-      console.log("config, before exec");
-    },
-    before_fork: function(config, wid) {
-      console.log("config, before fork");
-    },
-    after_fork: function(config, pid, ppid, wid) {
-      console.log("config, after fork: %d", process.pid);
-      oldbinQuit(config, pid, ppid, wid);
-    }
+    pidfile: __dirname + "/pidfile.pid"
   };
 
 };
